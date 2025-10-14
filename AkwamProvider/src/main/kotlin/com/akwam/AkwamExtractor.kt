46a629156bb1d8f4ea66650710c8e2633c9c443d
kim20598 <ki688494@gmail.com>
2025-10-14 20:25:09 +0100

    Update AkwamExtractor.kt

--- AkwamProvider/src/main/kotlin/com/akwam/AkwamExtractor.kt
+++ AkwamProvider/src/main/kotlin/com/akwam/AkwamExtractor.kt
@@ -1,35 +1,36 @@
 package com.akwam
 
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.*
 import org.jsoup.Jsoup
 
-object AkwamExtractor {
-    suspend fun extractLinks(
-        url: String,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-        try {
-            val doc = app.get(url).document
-            val links = doc.select("a.download-link")
+object AkwamExtractor : ExtractorApi() {
+    override val name = "Akwam"
+    override val mainUrl = "https://akwam.to"
+    override val requiresReferer = false
 
-            for (a in links) {
-                val link = a.attr("href")
-                val quality = Qualities.Unknown.value
-                callback(
-                    newExtractorLink(
-                        source = "Akwam",
-                        name = "Akwam Server",
-                        url = link,
-                        type = ExtractorLinkType.Direct
-                    ) {
-                        this.quality = quality
-                    }
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
+        val links = mutableListOf<ExtractorLink>()
+
+        val doc = app.get(url).document
+        val elements = doc.select("a.download-btn")
+
+        for (el in elements) {
+            val linkUrl = el.attr("href")
+            val quality = el.text().trim()
+            links.add(
+                ExtractorLink(
+                    name,
+                    name,
+                    linkUrl,
+                    mainUrl,
+                    getQualityFromName(quality),
+                    type = ExtractorLink.Type.DIRECT
                 )
-            }
-            return true
-        } catch (e: Exception) {
-            e.printStackTrace()
+            )
         }
-        return false
+
+        return links
     }
 }
