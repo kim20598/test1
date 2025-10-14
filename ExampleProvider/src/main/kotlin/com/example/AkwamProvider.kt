8934072a52353801bfb83ec6958a9ba940930d74
kim20598 <ki688494@gmail.com>
2025-10-14 11:47:31 +0100

    Update AkwamProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/AkwamProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/AkwamProvider.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.*
 
 class AkwamProvider : MainAPI() {
     override var mainUrl = "https://ak.sv"
-    override var name = "Akwam"
+    override var name = "Akwam-tesr"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
     override var lang = "ar"
 
