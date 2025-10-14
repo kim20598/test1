222cd5dfed769ea707bd3a34e3808aad0d877e36
kim20598 <ki688494@gmail.com>
2025-10-14 11:37:20 +0100

    Create AkwamProvider.kt

---
+++ ExampleProvider/src/main/kotlin/com/example/AkwamProvider.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import com.lagradost.cloudstream3.*
+
+class AkwamProvider : MainAPI() {
+    override var mainUrl = "https://ak.sv"
+    override var name = "Akwam"
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+    override var lang = "ar"
+
+    override val hasMainPage = false // Disable main page for now
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        // Return empty for testing
+        return emptyList()
+    }
+}
