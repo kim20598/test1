fa318894915852272c0448f2c22e3f3a894ed2d7
kim20598 <ki688494@gmail.com>
2025-11-06 20:13:02 +0100

    Refactor Akwam class into AkwamProvider plugin

--- akwam/src/main/kotlin/com/akwam/AkwamProvider.kt
+++ akwam/src/main/kotlin/com/akwam/AkwamProvider.kt
@@ -1,40 +1,12 @@
 package com.akwam
 
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 
-class Akwam : MainAPI() {
-    override var mainUrl = "https://ak.sv"
-    override var name = "Akwam"
-    override val hasMainPage = true
-    override var lang = "ar"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-
-    override val mainPage = mainPageOf(
-        "$mainUrl/movies" to "الأفلام",
-        "$mainUrl/series" to "المسلسلات"
-    )
-
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val doc = app.get(request.data).document
-        val movies = doc.select("div.movieBox, div.seriesBox").mapNotNull {
-            val link = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
-            val title = it.selectFirst("h3")?.text() ?: "No title"
-            val poster = it.selectFirst("img")?.attr("data-src") ?: ""
-            MovieSearchResponse(title, link, this.name, TvType.Movie, poster)
-        }
-        return newHomePageResponse(request.name, movies)
-    }
-
-    override suspend fun load(url: String): LoadResponse {
-        val doc = app.get(url).document
-        val title = doc.selectFirst("h1")?.text() ?: "Akwam Video"
-        val poster = doc.selectFirst("meta[property=og:image]")?.attr("content")
-        val description = doc.selectFirst("meta[property=og:description]")?.attr("content")
-
-        return newMovieLoadResponse(title, url, TvType.Movie, url) {
-            this.posterUrl = poster
-            this.plot = description
-        }
+@CloudstreamPlugin
+class AkwamProvider : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Akwam())
     }
 }
