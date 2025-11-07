0084406989b9d0afed9deb42a5aa6f9f75ee3c00
kim20598 <ki688494@gmail.com>
2025-11-07 13:11:28 +0100

    Update Akwam.kt

--- akwam/src/main/kotlin/com/akwam/Akwam.kt
+++ akwam/src/main/kotlin/com/akwam/Akwam.kt
@@ -2,6 +2,7 @@ package com.akwam
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.extractors.*
 import org.jsoup.nodes.Element
 
 class Akwam : MainAPI() {
