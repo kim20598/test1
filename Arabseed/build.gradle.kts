57d6afc5b4d5355be6467e7a8190efb146422e58
kim20598 <ki688494@gmail.com>
2025-11-08 17:02:12 +0100

    Update build.gradle.kts

--- Arabseed/build.gradle.kts
+++ Arabseed/build.gradle.kts
@@ -1,3 +1,34 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+android {
+    namespace = "com.arabseed"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-XXLanguage:+BreakContinueInInlineLambdas")
+    }
+}
+
 // use an integer for version numbers
 version = 4
 
@@ -23,8 +54,10 @@ cloudstream {
     )
 
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://a.asd.homes&size=%size%"
-    
-    dependencies {
+
+    isCrossPlatform = true
+
+dependencies {
     val cloudstream by configurations
 
     implementation("com.google.android.material:material:1.13.0")
@@ -32,5 +65,5 @@ cloudstream {
     implementation("androidx.room:room-ktx:2.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
-    isCrossPlatform = true
+    cloudstream("com.lagradost:cloudstream3:pre-release")
 }
