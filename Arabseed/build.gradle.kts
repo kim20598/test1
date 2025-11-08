885eb2a3c0e351dec05b38a9d435d0be43a616a2
kim20598 <ki688494@gmail.com>
2025-11-08 17:51:40 +0100

    Update build.gradle.kts

--- Arabseed/build.gradle.kts
+++ Arabseed/build.gradle.kts
@@ -1,3 +1,6 @@
+// use an integer for version numbers
+version = 4
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -31,10 +34,35 @@ android {
 
 dependencies {
     val cloudstream by configurations
+
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.browser:browser:1.9.0")
     implementation("androidx.room:room-ktx:2.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     cloudstream("com.lagradost:cloudstream3:pre-release")
+}
+
+cloudstream {
+    language = "ar"
+
+    authors = listOf("kim20598")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     */
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://a.asd.homes&size=%size%"
+    isCrossPlatform = true
 }
