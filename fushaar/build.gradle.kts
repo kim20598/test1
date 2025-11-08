a6f2f76d2afbd46a560c277b38d4401921e18f0b
kim20598 <ki688494@gmail.com>
2025-11-08 16:25:53 +0100

    Update build.gradle.kts

--- fushaar/build.gradle.kts
+++ fushaar/build.gradle.kts
@@ -1,41 +1,26 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
-}
+// use an integer for version numbers
+version = 4
 
-repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
-}
 
-android {
-    namespace = "com.fushaar"
-    compileSdk = 34
+cloudstream {
+    language = "ar"
+    // All of these properties are optional, you can safely remove them
 
-    defaultConfig {
-        minSdk = 21
-    }
+    authors = listOf("kim20598")
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movie",
+    )
 
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs = listOf("-XXLanguage:+BreakContinueInInlineLambdas")
-    }
-}
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://fushaar.com&size=%size%"
 
-dependencies {
-    val cloudstream by configurations
-
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.browser:browser:1.9.0")
-    implementation("androidx.room:room-ktx:2.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
-    cloudstream("com.lagradost:cloudstream3:pre-release")
+    isCrossPlatform = true
 }
