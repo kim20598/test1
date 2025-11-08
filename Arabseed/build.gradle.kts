1033a89440b6829b0afc4b628378c56d0b528026
kim20598 <ki688494@gmail.com>
2025-11-08 17:41:12 +0100

    Update build.gradle.kts

--- Arabseed/build.gradle.kts
+++ Arabseed/build.gradle.kts
@@ -1,43 +1,28 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+// use an integer for version numbers
+version = 4
+
+
+cloudstream {
+    language = "ar"
+    // All of these properties are optional, you can safely remove them
+
+    authors = listOf("kim20598")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movie",
+        "TvSerise",
+        " Anime"
+    )
+
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://a.asd.homes&size=%size%"
+
+    isCrossPlatform = true
 }
-
-repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
-}
-
-android {
-    namespace = "com.arabseed"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs = listOf("-XXLanguage:+BreakContinueInInlineLambdas")
-    }
-}
-
-
-dependencies {
-    val cloudstream by configurations
-
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.browser:browser:1.9.0")
-    implementation("androidx.room:room-ktx:2.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
-    cloudstream("com.lagradost:cloudstream3:pre-release")
-}
-
