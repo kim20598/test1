2d9d82c167de37d2b7554d9fc5534d8f78089380
kim20598 <ki688494@gmail.com>
2025-11-15 15:26:58 +0100

    Update MovizlandProvider.kt

--- Movizland/src/main/kotlin/com/movizland/MovizlandProvider.kt
+++ Movizland/src/main/kotlin/com/movizland/MovizlandProvider.kt
@@ -5,8 +5,8 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class MovizlandProvider : Plugin() {
+class MovizLandProvider : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(Movizland())
+        registerMainAPI(MovizLand())
     }
 }
