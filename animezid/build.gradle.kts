04ef84eb072ceb0fde331a21f5e487a2ac6e29e1
kim20598 <ki688494@gmail.com>
2025-11-22 05:13:16 +0100

    Update build.gradle.kts

--- animezid/build.gradle.kts
+++ animezid/build.gradle.kts
@@ -1,16 +1,14 @@
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+}
+
+cloudstream {
+    authors = listOf("YourName")
+    language = "ar"
+    tvTypes = listOf("Anime", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=animezid.cam&sz=%size%"
 }
 
 dependencies {
     implementation("com.lagradost:cloudstream3-core:latest.release")
-}
-
-cloudstream {
-    language = "ar"
-    authors = listOf("kim20598")
-    status = 1
-    tvTypes = listOf("Anime", "Movie")
-    iconUrl = "https://www.google.com/s2/favicons?domain=animezid.cam&sz=%size%"
 }
