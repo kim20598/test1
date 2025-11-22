c384a483f0d5335087e570d4d6011ad917d8ebf0
kim20598 <ki688494@gmail.com>
2025-11-22 05:31:01 +0100

    Update build.gradle.kts

--- animezid/build.gradle.kts
+++ animezid/build.gradle.kts
@@ -1,5 +1,20 @@
 plugins {
-    id("java-library")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.animezid"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 cloudstream {
